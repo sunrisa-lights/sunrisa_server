d9415f5790a2ad23e7b459f9de71b1e491288fe5
sunrisa <sunrisalights@gmail.com>
2020-04-15 02:58:00 +0000

    Add rack flow + tests

--- tests/unit/app/models/rack_test.py
+++ tests/unit/app/models/rack_test.py
@@ -1,19 +1,21 @@
 from app.models.rack import Rack
 
 def test_create_room():
-    rack = Rack(1, [1, 2, 3], False, False)
+    rack = Rack(1, 2, 40, False, False)
 
     assert rack.rack_id == 1
-    assert rack.shelve_ids == [1, 2, 3]
+    assert rack.room_id == 2
+    assert rack.voltage == 40
     assert rack.is_on == False
     assert rack.is_connected == False
 
 
 def test_create_room_from_json():
-    rack = Rack.from_json({'rack_id': 1, 'shelve_ids': [1, 2, 3]})
+    rack = Rack.from_json({'rack_id': 1, 'room_id': 2})
 
     assert rack.rack_id == 1
-    assert rack.shelve_ids == [1, 2, 3]
+    assert rack.room_id == 2
+    assert rack.voltage == 0 # auto-initialized to False if not present
     assert rack.is_on == False  # auto-initialized to False if not present
     assert rack.is_connected == False # auto-initialized to False if not present
 
