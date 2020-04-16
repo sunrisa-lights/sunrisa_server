1504bf34fcc55573cdeb2bae977f380420844171
sunrisa <sunrisalights@gmail.com>
2020-04-16 04:19:02 +0000

    Add more plant tests and db ops:

---
+++ tests/unit/app/db/plant_test.py
@@ -0,0 +1,35 @@
+from unittest.mock import MagicMock
+import pytest
+
+from app.models.plant import Plant
+from app.db.plant import create_plant_table, write_plant
+
+
+@pytest.fixture
+def mock_plant():
+    return Plant.from_json({'olcc_number': 1, 'shelf_id': 2})
+
+
+def test_write_plant(mock_plant):
+    conn = MagicMock()
+
+    write_plant(conn, mock_plant)
+
+    sql = "INSERT INTO `plants` VALUES (%s, %s) ON DUPLICATE KEY UPDATE shelf_id=%s"
+    conn.cursor().execute.assert_called_with(sql, (mock_plant.olcc_number, mock_plant.shelf_id, mock_plant.shelf_id))
+
+def test_create_plant_table():
+    conn = MagicMock()
+
+    create_plant_table(conn)
+    sql = """CREATE TABLE IF NOT EXISTS plants(
+    olcc_number INT NOT NULL,
+    shelf_id INT,
+    PRIMARY KEY (olcc_number),
+    CONSTRAINT fk_shelf
+    FOREIGN KEY (shelf_id)
+        REFERENCES shelves(shelf_id)
+    );
+    """
+
+    conn.cursor().execute.assert_called_with(sql)
--- tests/unit/app/db/shelf_test.py
+++ tests/unit/app/db/shelf_test.py
@@ -17,7 +17,7 @@ def test_write_shelf(mock_shelf):
 
     conn.cursor().execute.assert_called_with("INSERT INTO `shelves` VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE recipe_id=%s", (mock_shelf.shelf_id, mock_shelf.rack_id, mock_shelf.recipe_id, mock_shelf.recipe_id))
 
-def test_create_room_table():
+def test_create_shelf_table():
     conn = MagicMock()
 
     create_shelf_table(conn)
