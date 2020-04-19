02004c68ce164b233af4773e93381e5cd0197449
sunrisa <sunrisalights@gmail.com>
2020-04-19 19:09:12 +0000

    Remove unused endpoints

--- sunrisa.py
+++ sunrisa.py
@@ -7,20 +7,14 @@ from app.views.events import init_event_listeners
 socketio = SocketIO()
 from app_config import AppConfig
 
-# blueprints
-from app.views.routes import simple_page
-
-blueprints = [simple_page]
 
 app = Flask(__name__)
-app.register_blueprint(simple_page)
 
 def create_app(debug):
     """Create an application."""
     app = Flask(__name__)
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
-    register_blueprints(app, blueprints)
 
     env = "development"
     app_config = AppConfig(socketio, env)
@@ -28,9 +22,6 @@ def create_app(debug):
     socketio.init_app(app)
     init_event_listeners(app_config, socketio)
     return app
-
-def register_blueprints(app, blueprints):
-    [app.register_blueprint(b) for b in blueprints]
 
 
 if __name__ == '__main__':
