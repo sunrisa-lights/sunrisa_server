f31ff3d59706b924325d72dc1fa0e22693ae58cc
sunrisa <sunrisalights@gmail.com>
2020-06-22 05:02:52 +0000

    Fix pytest

--- tests/unit/app/models/shelf_test.py
+++ tests/unit/app/models/shelf_test.py
@@ -2,14 +2,10 @@ from app.models.shelf import Shelf
 
 
 def test_create_shelf():
-    shelf = Shelf(1, 2, 3, 4, 5, 6)
+    shelf = Shelf(1, 2)
 
     assert shelf.shelf_id == 1
     assert shelf.rack_id == 2
-    assert shelf.recipe_id == 3
-    assert shelf.power_level == 4
-    assert shelf.red_level == 5
-    assert shelf.blue_level == 6
 
 
 def test_create_shelf_from_json():
@@ -17,7 +13,3 @@ def test_create_shelf_from_json():
 
     assert shelf.shelf_id == 1
     assert shelf.rack_id == 2
-    assert shelf.recipe_id == None
-    assert shelf.power_level == None
-    assert shelf.red_level == None
-    assert shelf.blue_level == None
