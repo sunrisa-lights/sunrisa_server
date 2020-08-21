c3890d6532f290f299886b2beb28cf3332020784
Hodol Kim <hodolkim@Hodols-MacBook-Pro.local>
2020-08-21 15:25:30 -0400

    address comments

--- tests/unit/app/validations/start_grows_for_shelves_test.py
+++ tests/unit/app/validations/start_grows_for_shelves_test.py
@@ -2,11 +2,11 @@ from app.validation.start_grows_for_shelves import validate_grow_id
 from unittest.mock import MagicMock
 import pytest
 
-
+@pytest.fixture
 def test_validate_grow_id_fail():
     AppConfig = MagicMock()
     grow_id = 4
-    assert validate_grow_id(AppConfig, {"grow": {"blow": grow_id}}) != (True, str(grow_id))
+    assert validate_grow_id(AppConfig, {"grow": {"blow": grow_id}}) == (False, "Grow ID not included")
 
 
 def test_validate_grow_id_pass():
