8839b1ed47d0ad3c651c1b84cf88e1310412496d
Sunrisa Lights <sunrisalights@gmail.com>
2020-12-22 18:34:29 -0800

    Add comment

--- app/db/grow.py
+++ app/db/grow.py
@@ -19,6 +19,7 @@ def write_grow(conn, grow: Grow) -> Grow:
             grow.is_new_recipe,
             grow.tag_set,
             grow.nutrients,
+            # explicitly set this to None if it's not a valid number (0 isn't valid for this field either)
             grow.weekly_reps if grow.weekly_reps else None,
             grow.pruning_date_1,
             grow.pruning_date_2,
